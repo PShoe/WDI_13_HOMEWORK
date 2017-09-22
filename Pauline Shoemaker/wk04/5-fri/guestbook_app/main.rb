58e4ead8a22c5851e9a4a0a55a90e2dfd4c4a0f1
Pauline Shoemaker <shoemaker.pauline@gmail.com>
2017-09-22 13:40:35 +1000

    saving for reboot

---
+++ Pauline Shoemaker/wk04/5-fri/guestbook_app/main.rb
@@ -0,0 +1,16 @@
+
+require 'sinatra'
+require 'sinatra/reloader'
+
+comments = ['lol','looks great']
+
+get '/' do
+  @comments = comments
+  erb :index
+end
+
+post '/leave_comment' do
+  @comments = comments
+  comments.push(params[:body])
+  redirect '/'
+end
