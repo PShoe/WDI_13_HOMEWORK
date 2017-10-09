66ad4617245bcb60550d1983a497293bdc2a8d1d
Pauline Shoemaker <shoemaker.pauline@gmail.com>
2017-10-09 17:56:20 +1100

    saving at the bar

--- Pauline Shoemaker/wk07/1-mon/good_food_hunting/app/controllers/dishes_controller.rb
+++ Pauline Shoemaker/wk07/1-mon/good_food_hunting/app/controllers/dishes_controller.rb
@@ -18,4 +18,15 @@ class DishesController < ApplicationController
   def show
     @dish = Dish.find(params[:id])
   end
+  def edit
+    @dish = Dish.find(params[:id])
+  end
+  def update
+  dish = Dish.find(params[:id])
+  dish.name = params[:name]
+  dish.image_url = params[:image_url]
+  dish.save
+  redirect_to "/dishes/#{dish.id}"
+  end
+
 end
--- Pauline Shoemaker/wk07/1-mon/good_food_hunting/config/routes.rb
+++ Pauline Shoemaker/wk07/1-mon/good_food_hunting/config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
   resources :dishes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-# get '/tips', to: 'tips#index'
-# get '/tips/new', to: 'tips#new'
-# get '/tips/:id', to: 'tips#show'
-# get '/tips/:id/edit', to: 'tips#edit'
-# put '/tips/:id', to: 'tips#update'
-# post '/tips', to: 'tips#create'
 end
