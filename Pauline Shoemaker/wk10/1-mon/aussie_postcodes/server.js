bf33d766578e44860bc01a5fec4bb1690cb9a50d
Pauline Shoemaker <shoemaker.pauline@gmail.com>
2017-10-30 21:30:27 +1100

    added the csv to json postcode challenge

---
+++ Pauline Shoemaker/wk10/1-mon/aussie_postcodes/server.js
@@ -0,0 +1,31 @@
+var http = require('http');
+var fs = require('fs');
+
+var app = function(request, response) {
+
+  arrayObjects = []
+  fs.readFile('postcodes.csv', 'utf-8', function(err, data){
+    array = data.split('\n');
+    headers = array[0];
+    eachHeader = headers.split(',') // each
+    array.shift() //remove the headers
+
+    for(var i = 0; i < (array.length - 1); i++){
+      var eachLine = array[i].split(',');
+      object = {};
+      for(var j = 0; j < eachHeader.length; j++) {
+        var key = eachHeader[j];
+        var value = eachLine[j];
+        object[key] = value;
+      }
+      arrayObjects.push(object);
+    }
+  var myJSON = JSON.stringify(arrayObjects);
+  response.write(myJSON);
+  response.end();
+  })
+}
+
+
+var server = http.createServer( app );
+server.listen(8888);
